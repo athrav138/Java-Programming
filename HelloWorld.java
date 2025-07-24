5af5f3a02b728ad4dfe68d97cba83a10893a2b78
atharv138 <suryavanshiathrav072@gmail.com>
2025-07-24 20:39:11 +0530

    This is a Second commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld
+{
+  //public static void main(char args[]){
+public static void main (String[] args){
+    System.out.println("Hello World");
+    System.out.print("I am Atharv Milind Suryavanshi.");
+    
+  }
+
+}
---
+++ variables.java
@@ -0,0 +1,23 @@
+
+public class variables
+{
+
+  public static void main( String[] args){
+
+    int a = 12;
+    int b = 23;
+    int sum = a + b;
+    
+    System.out.print("The sum of a=12 and b=23 is: "+sum);
+    
+    String name = "Atharv Milind Suryavanshi.";
+    System.out.println(name);
+    
+    float percent = 95;
+    System.out.println("The percentage in float is:"+percent);
+    
+    double salary=20000.23;
+    System.out.println("The salary is: "+salary);
+
+  }
+}
