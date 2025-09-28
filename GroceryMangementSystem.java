5571ce9be14c3e637b238aeab8a1d8d41eae6dff
atharv138 <suryavanshiathrav072@gmail.com>
2025-09-28 20:16:37 +0530

    This is Grocery Mangement System

--- GroceryMangementSystem.java
+++ GroceryMangementSystem.java
@@ -1,37 +1,47 @@
+import java.util.Scanner; 
 
- abstract class GroceryItem
-{
-  private String name;
-  private double pricePerKg;
-  private double quantityKg;
+abstract class GroceryItem {
+    private String name;
+    private double pricePerKg;
+    private double quantityKg;
 
-  GroceryItem(String n,double p,double q)
-  {
-    name = n;
-    pricePerKg = p;
-    quantityKg = q;
-  }
+    public GroceryItem(String n, double p, double q) {
+        name = n;
+        pricePerKg = p;
+        quantityKg = q;
+    }
 
-   public  void display()
-  {
-    System.out.println("Name: "+name);
-    System.out.println("Price per kg: "+pricePerKg);
-    System.out.println("Quantity per Kg: "+quantityKg);
-
-  }
-
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Price per kg: " + pricePerKg);
+        System.out.println("Quantity per Kg: " + quantityKg);
+    }
 }
 
-class Fruit //extends GroceryItem
-{
+class Fruit extends GroceryItem {
+  private int isseasonal;
+  Scanner sc = new Scanner (System.in);
+    public Fruit(String n, double p, double q) {
+        super(n, p, q);
+        System.out.println("Enter the fruit is seasonal or not(1/0): ");
+        isseasonal = sc.nextInt();
+    }
 
-  
+    @Override
+    public void display() {
+        System.out.println("--- FRUIT ---");
+        super.display();
+        System.out.println("Is seasonal: "+(isseasonal)?"True":"False");
+    }
 }
 
-public class GroceryMangementSystem 
-{
-  public static void main(String[] args)
-  {
 
-  }
+
+public class GroceryMangementSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Fruit apple = new Fruit("Apple", 150.0, 2.5);
+        apple.display();
+    }
 }
