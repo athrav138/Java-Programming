6bd02d68be0be7687c5b4f92020de3aeb6aeaa59
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-06 20:44:04 +0530

    commit

--- String3.java
+++ String3.java
@@ -5,6 +5,23 @@ public class String3 {
   {
     Scanner sc = new Scanner(System.in);
 
-   
+   String s;
+   System.out.print("Enter string: ");
+   s = sc.next();
+
+   String result = "";
+   for(int i=0;i<s.length();i++)
+   {
+      
+      if(s.charAt(i)=='e')
+      {
+       result += 'i';
+
+      } else {
+      result += s.charAt(i);
+       }
+   }
+    System.out.println("Original: "+s);
+       System.out.println("Changes: "+result);
   }
 }
