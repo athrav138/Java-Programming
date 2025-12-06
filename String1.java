658d764e95d6b43aa09b6c3e303a36deed4a767b
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-06 20:16:53 +0530

    commit

---
+++ String1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class String1
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+
+    String s = "12345";
+
+    int number = Integer.parseInt(s);
+
+    System.out.println("Integer is: "+number);
+  }
+}
