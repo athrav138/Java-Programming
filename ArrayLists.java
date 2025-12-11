a431a8db82f33f58e17fe97485fada31a873976c
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-11 16:27:38 +0530

    arraylist

--- ArrayLists.java
+++ ArrayLists.java
@@ -8,14 +8,23 @@ public class ArrayLists
     Scanner sc = new Scanner(System.in);
 
     ArrayList<Integer> numbers = new ArrayList<Integer>();
-    ArrayList<Float> Decimals = new ArrayList<Float>();
+    ArrayList<String> names = new ArrayList<String>();
 
-    
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(4);
+    numbers.add(5);
 
+    names.add("Atharv");
+    names.add ("Rucha");
+    names.add("Sujata");
+    names.add("Milind");
+    names.add("chabu");
+    names.add("Tukaram");
 
-    
+    System.out.println("The list of Integers is: "+numbers);
+    System.out.println("THe list of Names is: "+names);
 
-
-    
   }
 }
