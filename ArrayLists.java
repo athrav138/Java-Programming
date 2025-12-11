a72dcd619b6efb6d658d39b3120583a57bef32b6
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-11 16:44:37 +0530

    Array Lists with Numbers and Strings

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+
 
 public class ArrayLists
 {
@@ -10,6 +12,7 @@ public class ArrayLists
     ArrayList<Integer> numbers = new ArrayList<Integer>();
     ArrayList<String> names = new ArrayList<String>();
 
+    // Add
     numbers.add(1);
     numbers.add(2);
     numbers.add(3);
@@ -26,5 +29,42 @@ public class ArrayLists
     System.out.println("The list of Integers is: "+numbers);
     System.out.println("THe list of Names is: "+names);
 
-  }
+    // Get 
+    System.out.println("Get : "+numbers.get(2));
+    System.out.println("Get String: "+names.get(1));
+
+    // Add
+
+    numbers.add(1,23);
+    System.out.println("Array list of numbers: "+numbers);
+
+    // Set element
+
+    numbers.set(2,12345);
+
+        System.out.println("Array list of numbers: "+numbers);
+
+
+        numbers.remove(2);
+        System.out.println("Array list of numbers: "+numbers);
+
+        // Size of array list
+        System.out.println("THe size of arrayList of numbers: "+numbers.size());
+
+        // Loops
+
+        for(int i=0;i<numbers.size();i++)
+        {
+          System.out.print(" "+numbers.get(i));
+        }
+
+        // sorting
+        Collections.sort(numbers);
+
+        System.out.println(numbers);
+        //Sorting strinng array list
+        Collections.sort(names);
+        System.out.println(names);
+
+  } 
 }
