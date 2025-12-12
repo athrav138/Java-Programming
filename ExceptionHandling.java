25ab2c36908796180560acbb3a437759b51d12a9
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-12 20:07:07 +0530

    Exception Hangling using try catch

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,12 +1,19 @@
-import java.util.*;
+import java.util.Scanner;
 
+class MyException extends Exception
+{
+    MyException (String message)
+    {
+        super(message);
+    }
+}
 public class ExceptionHandling
 {
     public static int divide(int a, int b)
     {
         int div;
         try
-        { s
+        { 
             div = a / b;
             return div;
         }
@@ -15,12 +22,21 @@ public class ExceptionHandling
             System.out.println("Divide by zero is not allowed.");
             return -1;   // return a default value
         }
+        catch(Exception e)
+        {
+            System.out.println("Any other Exception");
+            return -1;
+        }
+        finally
+        {
+            System.out.println("It always runs");
+        }
     }
 
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Result: " + divide(19, 0));
+        System.out.println("Result: " + divide(19, 3));
     }
 }
