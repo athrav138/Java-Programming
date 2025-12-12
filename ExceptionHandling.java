b87ff42ff3bc632f59e6e5ebfad7c063935d0e5a
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-12 19:18:32 +0530

    Exception Handling

---
+++ ExceptionHandling.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ExceptionHandling
+{
+    public static int divide(int a, int b)
+    {
+        int div;
+        try
+        {
+            div = a / b;
+            return div;
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("Divide by zero is not allowed.");
+            return -1;   // return a default value
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Result: " + divide(19, 0));
+    }
+}
