26b543e4a56ec36d566b03ba1a8dc4a0bcc07ba1
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-13 13:15:30 +0530

    commit

---
+++ MultiThreading.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class MyThread extends Thread
+{
+
+}
+public class MultiThreading {
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+
+  }
+}
