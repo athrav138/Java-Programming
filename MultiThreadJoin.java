ff52cdaabb807fb2e3b8a21bdecd99516c50464c
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-13 13:21:38 +0530

    MultiThreadJoin

---
+++ MultiThreadJoin.java
@@ -0,0 +1,16 @@
+class JoinExample extends MultiThreadJoin {
+    public void run() {
+        for(int i = 1; i <= 3; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        JoinExample t1 = new JoinExample();
+        JoinExample t2 = new JoinExample();
+
+        t1.start();
+        t1.join();   // wait till t1 finishes
+        t2.start();
+    }
+}
