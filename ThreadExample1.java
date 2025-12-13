d76f32060df9d71aa42f5a76026407cd3a113f75
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-13 13:18:46 +0530

    Multithreading

---
+++ ThreadExample1.java
@@ -0,0 +1,19 @@
+class MyThread extends Thread {
+    public void run() {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("Thread 1: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class ThreadExample1 {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();  // starts new thread
+    }
+}
