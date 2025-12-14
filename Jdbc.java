11da108a4d17f5f3f0934a7da7a8746c00e69d61
atharv138 <suryavanshiathrav072@gmail.com>
2025-12-14 19:40:48 +0530

    commit

--- Jdbc.java
+++ Jdbc.java
@@ -1,9 +1,8 @@
-import java.sql;
 import java.util.Scanner;
 
 public class Jdbc
 {
-  public statc void main(String[] args)
+  public static void main(String[] args)
   {
     Scanner sc = new Scanner(System.in);
 
