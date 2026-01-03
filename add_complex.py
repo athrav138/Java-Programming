d43b4526a23b9f6b63c8899a4d230036597e08e6
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-03 22:04:10 +0530

    commit

--- add_complex.py
+++ add_complex.py
@@ -4,7 +4,7 @@ class add_complex:
     self.img  = img
 
   def display(self):
-    print(f"The comples is: {self.real}i + {self.img}j")
+    print(f"The complex is: {self.real}i + {self.img}j")
 
   """def add(self,c2):
     new_real = self.real + c2.real
@@ -16,11 +16,18 @@ class add_complex:
     new_img = self.img + c2.img
     return add_complex(new_real,new_img)
 
+  def __sub__(self,c2):
+    new_real = self.real - c2.real
+    new_img = self.img - c2.img
+    return add_complex(new_real,new_img)
+
 c1 =  add_complex(2,4)
 c2 =  add_complex(4,5)
 
 #c3 = c1.add(c2)
 c3 = c1 + c2
+c4 = c2 - c1
 c1.display()
 c2.display()
 c3.display()
+c4.display()
