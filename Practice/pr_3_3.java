d9cfc373e1d80915883f658fb319ff6fcf12a5d3
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-10 18:36:34 +0530

    commit

---
+++ Practice/pr_3_3.java
@@ -0,0 +1,24 @@
+public class pr_3_3 {
+    public static void main(String [] args)
+    {
+        char ch = 'c';
+
+        switch(ch){
+            case 'a':
+                System.out.println("THe character is 'a'");
+                break;
+
+            case 'b':
+                System.out.println("THe character is 'b'");
+                break;
+
+            case 'c':
+                System.out.println("THe character is 'c'");
+                break;
+
+            default :
+                System.out.println("The character other than 'a','b','c' ");
+
+        }
+    }
+}
