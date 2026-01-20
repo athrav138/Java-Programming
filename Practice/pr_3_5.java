e37ef41b900b9f3ba4292067a1e0c31763ef6567
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-20 19:21:17 +0530

    commit

---
+++ Practice/pr_3_5.java
@@ -0,0 +1,14 @@
+public class pr_3_5 {
+    public static void main(String [] args)
+    {
+        
+        System.out.print("\nTHe 1-20 numbers using do-while loop: ");
+        int j = 1;
+        do
+        {
+            System.out.print(" "+j);
+            j+=1;
+        }while(j<20 || j==20);
+
+        }
+    }
