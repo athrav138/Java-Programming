b521433f7e930d4a184a6e027f995b376da9c297
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-20 19:20:28 +0530

    commit

---
+++ Practice/pr_3_1.java
@@ -0,0 +1,20 @@
+public class pr_3_1 {
+    public static void main(String [] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else if(a>=b){
+            System.out.println("a is greater than equal to b");
+        }
+        else if(a<b){
+            System.out.println("a is less than b");
+        }
+        else {
+            System.out.println("a is not equal to b");
+        }
+    }
+}
