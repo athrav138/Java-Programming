bae41da44b9894c0e77978698b5b925581613784
atharv138 <suryavanshiathrav072@gmail.com>
2026-01-20 19:21:24 +0530

    commit

---
+++ Practice/pr_3_2.java
@@ -0,0 +1,13 @@
+public class pr_3_2 {
+    public static void main(String [] args)
+    {
+        int a = 10;
+        
+        if(a%2==0){
+            System.out.println("a is even number: "+a);
+        }
+        else{
+            System.out.println("a is odd number: "+a);
+        }
+    }
+}
